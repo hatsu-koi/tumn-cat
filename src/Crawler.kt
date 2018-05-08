df26d41e84e749ee385393bbc293d52fecafc090
onebone <jyc00410@gmail.com>
2018-05-08 22:11:31 +0900

    Working on naver news crawling

--- src/Crawler.kt
+++ src/Crawler.kt
@@ -1,19 +1,103 @@
 package com.tumn.cat
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 
-abstract class Crawler {
+abstract class Crawler(
+		protected val userAgent: String = "Tumn-Cat_Bot/v1.0",
+		protected val interval: Long
+	) {
 	abstract fun crawl()
+
+	fun connectAndGet(uri: String): Document = Jsoup.connect(uri).userAgent(userAgent).get()
 }
 
-data class Content(val type: String, val url: String, val content: String, val date: Date)
+data class Content(val uri: String, val content: String)
 
-val NAVER_NEWS_URI = ""
+val NAVER_NEWS_HEADLINES_URI: String = "http://news.naver.com/main/history/mainnews/index.nhn?date=%s&time=00:00"
 
-class NaverNewsCrawler: Crawler() {
+class NaverNewsCrawler(
+		userAgent: String,
+		interval: Long,
+		private val from: Date,
+		private val to: Date
+	):
+		Crawler(userAgent, interval) {
+	init {
+		if(to.before(from)){
+			throw IllegalArgumentException("Invalid range of date given")
+		}
+	}
 
 	override fun crawl() {
+		val c = Calendar.getInstance()
+		c.time = from
+		while(c.time.before(to)){
+			println("Crawl %s".format(c.time.toString()))
+
+			getListForDate(c.time).forEach {
+				println("Found %s".format(it))
+
+				val uri = URI(it)
+				if(uri.host == "history.news.naver.com") {
+					getHistoryNewsList(it).forEach { history ->
+						println("History> %s".format(history))
+
+						getCommentsList(history).forEach(System.out::println) // TODO save to database
+					}
+				}else if(uri.host == "news.naver.com"){
+					println("Comment> %s".format(it))
+
+					getCommentsList(it).forEach(System.out::println) // TODO
+				}
+			}
+
+			Thread.sleep(interval)
+
+			c.add(Calendar.DATE, 1)
+		}
+	}
+
+	private fun getListForDate(date: Date): ArrayList<String> {
+		val list = arrayListOf<String>()
+
+		val formatter = SimpleDateFormat("yyyy-MM-dd")
+
+		connectAndGet(NAVER_NEWS_HEADLINES_URI.format(formatter.format(date))).run {
+			select("div.newsnow_tx_inner a").forEach {
+				it.attr("href")?.let { href -> list.add(href) }
+			}
+		}
+
+		return list
+	}
+
+	private fun getHistoryNewsList(uri: String): ArrayList<String> {
+		val list = arrayListOf<String>()
+
+		connectAndGet(uri).select("div.hissue_cnt a").forEach {
+			it.attr("href")?.let { href ->
+				if(URI(href).host == "news.naver.com") {
+					list.add(href)
+				}
+			}
+		}
+
+		return list
+	}
+
+	private fun getCommentsList(uri: String): ArrayList<Content> {
+		val list = arrayListOf<Content>()
+
+		connectAndGet(uri).select("span.u_cbox_contents").forEach {
+			list.add(Content(uri, it.text()))
+		}
+
+		return list
 	}
 
 	override fun toString(): String = "Naver News Crawler"
--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,43 @@
 package com.tumn.cat
 
+import com.beust.klaxon.Klaxon
 import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
 
-class Main {
-	fun main(args: Array<String>){
-		if(File("config.json").exists()) {
-
+fun main(args: Array<String>){
+	if(!File("config.json").exists()) {
+		Object::class.java.getResourceAsStream("/config.json").use {input ->
+			FileOutputStream(File("config.json")).use {
+				input.copyTo(it)
+			}
 		}
+	}
 
-		println("Hello Kotlin!")
+	val config = Klaxon().parse<Config>(File("config.json"))!!
+
+	print("""Crawled data will be saved to: %s
+
+		| Available crawling sites
+		|* 1> Naver News
+		|* Select website to crawl from: """.trimMargin().format(config.host))
+
+	when(readLine()!!){
+		"1" -> {
+			print("""The crawler will crawl comments from headlines between two dates.
+				|* Please provide the date to start with (yyyy-mm-dd):
+			""".trimMargin())
+
+			val formatter = SimpleDateFormat("yyyy-MM-dd")
+			val start = formatter.parse(readLine()!!)
+
+			print("* Please provide the date to end with (yyyy-mm-dd): ")
+			val end = formatter.parse(readLine()!!)
+
+			NaverNewsCrawler(config.userAgent, 200, start, end).crawl()
+		}
+		else -> println("Unavailable option selected. Exiting.")
 	}
 }
+
+data class Config(val host: String, val userAgent: String)
